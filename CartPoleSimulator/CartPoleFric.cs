3d12a97eced6bc508a23fc6698012e201e8ef412
Rafka86 <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-05-30 16:02:25 +0900

    いい感じに動く

--- CartPoleSimulator/CartPoleFric.cs
+++ CartPoleSimulator/CartPoleFric.cs
@@ -3,18 +3,19 @@ using Rafka.MathLib.Real;
 using Rafka.MathLib.Real.Numerics;
 
 namespace CartPoleSimulator {
-    public class CartPoleFric : ODE {
-        private const double mc = 2.0;
-        private const double mp = 150.0 + 54.0;
-        private const double g = 9.80665;
-        public const double l = 0.0745;
-		private const double muc = 0.53;
-		private const double mup = 0.4;
+	public class CartPoleFric : ODE {
+		private const double mc = 2.0;
+		private const double mp = 150.0 + 54.0;
+		private const double g = 9.80665;
+		public const double l = 0.0745;
+		private const double muc = 0.0;//0.53;
+		private const double mup = 2.0;//0.4;
 
-        private const double div43 = 4.0 / 3.0;
-        private const double mc_mp = mc + mp;
-        private const double mpl = mp * l;
+		private const double div43 = 4.0 / 3.0;
+		private const double mc_mp = mc + mp;
+		private const double mpl = mp * l;
 
+		public double Nc { get; set; } = mc_mp * g;
 		public double F { get; set; } = 0.0;
 
 		public CartPoleFric() {
@@ -22,51 +23,54 @@ namespace CartPoleSimulator {
 		}
 
 		public void Init() {
-			x0 = new Vector(0.0, 0.0, 0.0, 0.0, (mc + mp) * g);
-            ts = 0.0;
-            te = 1.0;
-            F = 0.0;
+			x0 = new Vector(0.0, 0.0, 0.0, 0.0);
+			ts = 0.0;
+			te = 1.0;
+			F = 0.0;
+			Nc = mc_mp * g;
 		}
 
-		private double theta_2nd(double nc, double v, double theta, double theta_v) {
+		public void UpdateFriction(Vector x) {
+			Nc = mc_mp * g - mpl * (theta_2nd(x[1], x[2], x[3]) * Math.Sin(x[2]) + x[3] * x[3] * Math.Cos(x[2]));
+		}
+
+		private double theta_2nd(double v, double theta, double theta_v) {
 			double sin = Math.Sin(theta);
 			double cos = Math.Cos(theta);
 
 			double term1 = g * sin;
-			double term2_1 = (-F - mpl * theta_v * theta_v * (sin + muc * Math.Sign(nc * v) * cos)) / mc_mp;
-			double term2_2 = muc * g * Math.Sign(nc * v);
+			double term2_1 = (-F - mpl * theta_v * theta_v * (sin + muc * Math.Sign(Nc * v) * cos)) / mc_mp;
+			double term2_2 = muc * g * Math.Sign(Nc * v);
 			double term2 = cos * (term2_1 + term2_1);
 			double term3 = (mup * theta_v) / mpl;
 			double numer = term1 + term2 - term3;
 
-			double term4 = div43 - ((mp * cos) * (cos - muc * Math.Sign(nc * v))) / mc_mp;
+			double term4 = div43 - ((mp * cos) * (cos - muc * Math.Sign(Nc * v))) / mc_mp;
 			double denom = l * term4;
 
 			return numer / denom;
 		}
 
-		private double x_2nd(double nc, double v, double theta, double theta_v, double theta_2) {
+		private double x_2nd(double v, double theta, double theta_v, double theta_2) {
 			double sin = Math.Sin(theta);
 			double cos = Math.Cos(theta);
 
-            double numer = F + mpl * (theta_v * theta_v * sin - theta_2 * cos) - muc * nc * Math.Sin(nc * v);
+			double numer = F + mpl * (theta_v * theta_v * sin - theta_2 * cos) - muc * Nc * Math.Sign(Nc * v);
 
 			return numer / mc_mp;
 		}
 
 		public override Vector Feval(double t, Vector x) {
-			double nc   = x[4];
-			double p    = x[0];
-			double v    = x[1];
-			double th   = x[2];
+			double p = x[0];
+			double v = x[1];
+			double th = x[2];
 			double th_v = x[3];
 
 			Vector res = new Vector(x.Length);
-			res[3] = theta_2nd(nc, v, th, th_v);
-            res[2] = th_v;
-            res[1] = x_2nd(nc, v, th, th_v, res[3]);
-            res[0] = v;
-            res[4] = mc_mp * g - mpl * (res[3] * Math.Sin(th) + th_v * th_v * Math.Cos(th));
+			res[3] = theta_2nd(v, th, th_v);
+			res[2] = th_v;
+			res[1] = x_2nd(v, th, th_v, res[3]);
+			res[0] = v;
 
 			return res;
 		}
--- CartPoleSimulator/Program.cs
+++ CartPoleSimulator/Program.cs
@@ -7,223 +7,219 @@ using Rafka.MathLib.Real.Numerics;
 using static System.Console;
 using static System.Math;
 
-namespace CartPoleSimulator
-{
-    enum Command
-    {
-        GET,
-        MOV,
-        STP,
-        RST,
-    }
+namespace CartPoleSimulator {
+	enum Command {
+		GET,
+		MOV,
+		STP,
+		RST,
+	}
 
-    struct Packet
-    {
-        public long command;
-        public double[] data;
-    }
+	struct Packet {
+		public long command;
+		public double[] data;
+	}
 
-    class Program
-    {
-        const short port = 17997;
+	class Program {
+		const int port = 50000;
 
-        private static void Echo(Socket client)
-        {
-            var buf = new byte[256];
+		private static void Echo(Socket client) {
+			var buf = new byte[256];
 
-            while (client.Receive(buf) > 0) {
-                var packet = new Packet() {
-                    command = BitConverter.ToInt64(buf, 0),
-                    data = new double[4]
-                };
-                for (int i = 0; i < packet.data.Length; i++)
-                    packet.data[i] = BitConverter.ToDouble(buf, 8 * (i + 1));
-                switch ((Command)packet.command) {
-                    case Command.GET: {
-                            Error.WriteLine("Command : GET");
-                            packet.data[0] = packet.data[1] = packet.data[2] = packet.data[3] = 1.0;
-                            Buffer.BlockCopy(BitConverter.GetBytes(packet.command), 0, buf, 0, 8);
-                            for (int i = 0; i < packet.data.Length; i++)
-                                Buffer.BlockCopy(BitConverter.GetBytes(packet.data[i]), 0, buf, 8 * (i + 1), 8);
-                            if (client.Send(buf, 40, SocketFlags.None) <= 0)
-                                Error.WriteLine("Sending Error.");
-                        }
-                        break;
-                    case Command.MOV:
-                        Error.WriteLine("Command : MOV");
-                        break;
-                    case Command.STP:
-                        Error.WriteLine("Command : STP");
-                        break;
-                    case Command.RST:
-                        Error.WriteLine("Command : RST");
-                        break;
-                    default:
-                        return;
-                }
-            }
-        }
+			while (client.Receive(buf) > 0) {
+				var packet = new Packet() {
+					command = BitConverter.ToInt64(buf, 0),
+					data = new double[4]
+				};
+				for (int i = 0; i < packet.data.Length; i++)
+					packet.data[i] = BitConverter.ToDouble(buf, 8 * (i + 1));
+				switch ((Command)packet.command) {
+					case Command.GET: {
+							Error.WriteLine("Command : GET");
+							packet.data[0] = packet.data[1] = packet.data[2] = packet.data[3] = 1.0;
+							Buffer.BlockCopy(BitConverter.GetBytes(packet.command), 0, buf, 0, 8);
+							for (int i = 0; i < packet.data.Length; i++)
+								Buffer.BlockCopy(BitConverter.GetBytes(packet.data[i]), 0, buf, 8 * (i + 1), 8);
+							if (client.Send(buf, 40, SocketFlags.None) <= 0)
+								Error.WriteLine("Sending Error.");
+						}
+						break;
+					case Command.MOV:
+						Error.WriteLine("Command : MOV");
+						break;
+					case Command.STP:
+						Error.WriteLine("Command : STP");
+						break;
+					case Command.RST:
+						Error.WriteLine("Command : RST");
+						break;
+					default:
+						return;
+				}
+			}
+		}
 
-        static bool turnover, repeat;
-        static CartPoleFric cp;
-        static Vector x;
-        const double power = 5.0;
+		static bool turnover, repeat;
+		static CartPoleFric cp;
+		static Vector x;
+		const double power = 5000.0;
 
-        private class AsyncStateObject
-        {
-            public Socket Socket;
-            public byte[] ReceiveBuffer;
+		private class AsyncStateObject {
+			public Socket Socket;
+			public byte[] ReceiveBuffer;
 
-            public AsyncStateObject(Socket soc)
-            {
-                Socket = soc;
-                ReceiveBuffer = new byte[256];
-            }
-        }
+			public AsyncStateObject(Socket soc) {
+				Socket = soc;
+				ReceiveBuffer = new byte[256];
+			}
+		}
 
-        private static AsyncStateObject StartReceive(Socket soc)
-        {
-            var so = new AsyncStateObject(soc);
-            soc.BeginReceive(so.ReceiveBuffer,
-                0,
-                so.ReceiveBuffer.Length,
-                SocketFlags.None,
-                new AsyncCallback(ReceiveDataCallback),
-                so);
-            return so;
-        }
+		private static AsyncStateObject StartReceive(Socket soc) {
+			var so = new AsyncStateObject(soc);
+			soc.BeginReceive(so.ReceiveBuffer,
+					0,
+					so.ReceiveBuffer.Length,
+					SocketFlags.None,
+					new AsyncCallback(ReceiveDataCallback),
+					so);
+			return so;
+		}
 
-        private static void ReceiveDataCallback(IAsyncResult ar)
-        {
-            var so = (AsyncStateObject)ar.AsyncState;
+		private static void ReceiveDataCallback(IAsyncResult ar) {
+			var so = (AsyncStateObject)ar.AsyncState;
 
-            var len = 0;
-            try {
-                len = so.Socket.EndReceive(ar);
-            }
-            catch (ObjectDisposedException) {
-                Error.WriteLine("Closed.");
-                return;
-            }
+			var len = 0;
+			try {
+				len = so.Socket.EndReceive(ar);
+			}
+			catch (ObjectDisposedException) {
+				Error.WriteLine("Closed.");
+				return;
+			}
 
-            if (len <= 0) {
-                Error.WriteLine("Disconnected.");
-                so.Socket.Close();
-                return;
-            }
+			if (len <= 0) {
+				Error.WriteLine("Disconnected.");
+				so.Socket.Close();
+				return;
+			}
 
-            if (so.Socket.Available == 0) {
-                var p = new Packet() {
-                    command = BitConverter.ToInt64(so.ReceiveBuffer, 0),
-                    data = new double[4]
-                };
+			if (so.Socket.Available == 0) {
+				var p = new Packet() {
+					command = BitConverter.ToInt64(so.ReceiveBuffer, 0),
+					data = new double[4]
+				};
 
-                switch ((Command)p.command) {
-                    case Command.GET: {
-                            Error.WriteLine("Command : GET");
-                            if (!turnover) {
-                                Buffer.BlockCopy(BitConverter.GetBytes(p.command), 0, so.ReceiveBuffer, 0, 8);
-                                for (int i = 0; i < p.data.Length; i++)
-                                    Buffer.BlockCopy(BitConverter.GetBytes(x[i]), 0, so.ReceiveBuffer, 8 * (i + 1), 8);
-                                if (so.Socket.Send(so.ReceiveBuffer, 40, SocketFlags.None) <= 0)
-                                    Error.WriteLine("Sending Error.");
-                            } else {
-                                Buffer.BlockCopy(BitConverter.GetBytes((long)Command.RST), 0, so.ReceiveBuffer, 0, 8);
-                                for (int i = 0; i < p.data.Length; i++)
-                                    Buffer.BlockCopy(BitConverter.GetBytes(x[i]), 0, so.ReceiveBuffer, 8 * (i + 1), 8);
-                                if (so.Socket.Send(so.ReceiveBuffer, 40, SocketFlags.None) <= 0)
-                                    Error.WriteLine("Sending Error.");
-                            }
-                        }
-                        break;
-                    case Command.MOV: {
-                            if (!turnover) {
-                                Error.WriteLine("Command : MOV");
-                                cp.F = (BitConverter.ToDouble(so.ReceiveBuffer, 8) < 0) ? power : -power;
-                            } else {
-                                repeat = false;
-                                turnover = false;
-                            }
-                        }
-                        break;
-                    case Command.STP: {
-                            if (!turnover) {
-                                Error.WriteLine("Command : STP");
-                                cp.F = 0.0;
-                            } else {
-                                repeat = false;
-                                turnover = false;
-                            }
-                        }
-                        break;
-                    case Command.RST: {
-                            repeat = true;
-                            turnover = false;
-                        }
-                        break;
-                    default:
-                        return;
-                }
-            }
+				switch ((Command)p.command) {
+					case Command.GET: {
+							Error.WriteLine("Command : GET");
+							if (!turnover) {
+								Buffer.BlockCopy(BitConverter.GetBytes(p.command), 0, so.ReceiveBuffer, 0, 8);
+								for (int i = 0; i < p.data.Length; i++)
+									Buffer.BlockCopy(BitConverter.GetBytes(x[i]), 0, so.ReceiveBuffer, 8 * (i + 1), 8);
+								if (so.Socket.Send(so.ReceiveBuffer, 40, SocketFlags.None) <= 0)
+									Error.WriteLine("Sending Error.");
+							} else {
+								Buffer.BlockCopy(BitConverter.GetBytes((long)Command.RST), 0, so.ReceiveBuffer, 0, 8);
+								for (int i = 0; i < p.data.Length; i++)
+									Buffer.BlockCopy(BitConverter.GetBytes(x[i]), 0, so.ReceiveBuffer, 8 * (i + 1), 8);
+								if (so.Socket.Send(so.ReceiveBuffer, 40, SocketFlags.None) <= 0)
+									Error.WriteLine("Sending Error.");
+							}
+						}
+						break;
+					case Command.MOV: {
+							if (!turnover) {
+								Error.WriteLine("Command : MOV");
+								var pow = BitConverter.ToDouble(so.ReceiveBuffer, 8);
+								cp.F = -pow;
+								Error.WriteLine("Move power : " + cp.F);
+							} else {
+								repeat = false;
+								turnover = false;
+							}
+						}
+						break;
+					case Command.STP: {
+							if (!turnover) {
+								Error.WriteLine("Command : STP");
+								cp.F = 0.0;
+							} else {
+								repeat = false;
+								turnover = false;
+							}
+						}
+						break;
+					case Command.RST: {
+							repeat = true;
+							turnover = false;
+						}
+						break;
+					default:
+						return;
+				}
+			}
 
-            so.Socket.BeginReceive(so.ReceiveBuffer,
-                0,
-                so.ReceiveBuffer.Length,
-                SocketFlags.None,
-                new AsyncCallback(ReceiveDataCallback),
-                so);
-        }
+			so.Socket.BeginReceive(so.ReceiveBuffer,
+					0,
+					so.ReceiveBuffer.Length,
+					SocketFlags.None,
+					new AsyncCallback(ReceiveDataCallback),
+					so);
+		}
 
-        public static void Main()
-        {
-            var gp = new Gnuplot(@"C:\Program Files\gnuplot\bin\gnuplot.exe");
-            cp = new CartPoleFric();
+		public static void Main() {
+			var gp = new Gnuplot(@"C:\Program Files\gnuplot\bin\gnuplot.exe");
+			cp = new CartPoleFric();
 
-            gp.Start();
-            //gp.StandardInput.WriteLine("set size square");
-            gp.SetXRange(-0.5, 0.5);
-            gp.SetXLabel();
-            gp.SetYRange(0.0, 0.1);
+			gp.Start();
+			gp.StandardInput.WriteLine("set size square");
+			gp.SetXRange(-1.0, 1.0);
+			gp.SetXLabel();
+			gp.SetYRange(0.0, 2.0);
 
-            var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            var point = new IPEndPoint(IPAddress.Any, port);
-            sock.Bind(point);
-            sock.Listen(1);
-            Error.WriteLine("Listening");
-            var client = sock.Accept();
-            //Echo(client);
-            StartReceive(client);
-        
-            ODESolver.dt = 1e-3;
-            double deg_80 = 4.0 * PI / 9.0;
-            var t = 0;
-            repeat = true;
-            while (repeat) {
+			var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			var point = new IPEndPoint(IPAddress.Any, port);
+			sock.Bind(point);
+			sock.Listen(1);
+			Error.WriteLine("Listening");
+			var client = sock.Accept();
+			Error.WriteLine("Accepted.");
+			StartReceive(client);
 
-                t += 1;
-                cp.Init();
-                x = cp.x0;
-                //var step = 0;
-                while (-deg_80 < x[2] && x[2] < deg_80) {
-                    //cp.F *= (++step % 1000 == 0) ? -1 : 1;
-                    x = ODESolver.rk4Step(cp, 0.0, x);
-                    var pos = new Vector2(x[0], 0.0);
-                    var p_g = new Vector2(0.0, CartPole.l);
-                    p_g *= Matrix.RotationMatrix2D(x[2]);
-                    p_g += pos;
-                    gp.SetXLabelName("trial = " + t);
-                    gp.PlotLines(pos, p_g);
-                }
+			ODESolver.dt = 1e-4;
+			double deg_45 = PI / 4.0;
+			var t = 0;
+			repeat = true;
+			while (repeat) {
 
-                turnover = true;
-                while (turnover) ;
+				t += 1;
+				cp.Init();
+				x = cp.x0;
+				var count = 0;
+				while (-deg_45 < x[2] && x[2] < deg_45 && -1.0 < x[0] && x[0] < 1.0) {
+					x = ODESolver.rk4Step(cp, 0.0, x);
+					cp.UpdateFriction(x);
+					Error.WriteLine(x);
+					var pos = new Vector2(x[0], 0.0);
+					var p_g = new Vector2(0.0, CartPoleFric.l * 12.0);
+					p_g *= Matrix.RotationMatrix2D(x[2]);
+					p_g += pos;
+					gp.SetXLabelName("trial = " + t);
+					if (count++ % 10 == 0) {
+						cp.F = 0.0;
+						gp.PlotLines(pos, p_g);
+					}
+				}
 
-                WriteLine((repeat) ? "Retry." : "Finiish.");
-            }
+				turnover = true;
+				while (turnover) ;
 
-            client.Close();
-            sock.Close();
-            gp.Close();
-        }
-    }
+				WriteLine((repeat) ? "Retry." : "Finish.");
+			}
+
+			client.Close();
+			sock.Close();
+			gp.Close();
+		}
+	}
 }
